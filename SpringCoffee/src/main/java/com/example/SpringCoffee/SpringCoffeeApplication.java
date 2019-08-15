4446c87a08a25f4c35e16a5c2745432a4a5e409a
syl9798@163.com <syl9798@163.com>
2019-08-15 22:50:58 +0800

    tesst git

--- SpringCoffee/src/main/java/com/example/SpringCoffee/SpringCoffeeApplication.java
+++ SpringCoffee/src/main/java/com/example/SpringCoffee/SpringCoffeeApplication.java
@@ -1,13 +1,27 @@
 package com.example.SpringCoffee;
 
+import com.example.SpringCoffee.repository.CoffeeOrderRepository;
+import com.example.SpringCoffee.repository.CoffeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCoffeeApplication {
+public class SpringCoffeeApplication implements ApplicationRunner {
+
+	@Autowired
+	private CoffeeRepository coffeeRepository;
+	@Autowired
+	private CoffeeOrderRepository coffeeOrderRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoffeeApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+
+	}
 }
---
+++ SpringCoffee/src/main/java/com/example/SpringCoffee/repository/CoffeeRepository.java
@@ -0,0 +1,4 @@
+package com.example.SpringCoffee.repository;
+
+public class CoffeeRepository {
+}
